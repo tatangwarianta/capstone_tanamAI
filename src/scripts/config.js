9b4f3d95cb9f1c0d394ebbfc536a5a9d3fa0a9eb
Rayyan <60314224+RayyanHermanto@users.noreply.github.com>
2025-06-09 14:30:13 +0700

    a

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'https://story-api.dicoding.dev/v1',
-  VAPID_PUBLIC_KEY: 'BCCs2eonMI-6H2ctvFaWg-UYdDv387Vno_bzUzALpB442r2lCnsHmtrx8biyPi_E-1fSGABK_Qs_GlvPoJJqxbk',
+  BASE_URL: 'http://localhost:3000',
 };
 
 export default CONFIG;
