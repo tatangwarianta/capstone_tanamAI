28abe97fd629593d9077d639e168e745b79b5723
Rayyan <hermantorayyan@gmail.com>
2025-08-27 01:13:49 +0700

    .

--- src/scripts/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  BASE_URL: 'https://servercapstone-production.up.railway.app',
-  IMAGE_BASE_URL: 'https://servercapstone-production.up.railway.app/uploads', // sesuaikan dengan folder statis backend
-};
-
-export default CONFIG;
